aadca1eaa10415f7dd698cf71015e5e27940da84
Carlos Medina <carlos.medina@itexico.com>
2020-10-05 08:12:02 -0500

    Uploading initial files

---
+++ src/test/java/com/training/questionaire/qmanager/QmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.questionaire.qmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
