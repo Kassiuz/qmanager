2396bd80ef02a8edf6925490fd474c1e28393b02
Carlos Medina <carlos.medina@itexico.com>
2020-10-14 14:30:04 -0500

    Step 4 implementation

--- src/main/java/com/training/questionaire/qmanager/web/rest/HelloWorldRestController.java
+++ src/main/java/com/training/questionaire/qmanager/web/rest/HelloWorldRestController.java
@@ -2,10 +2,10 @@ package com.training.questionaire.qmanager.web.rest;
 
 
 import com.training.questionaire.qmanager.web.dto.ExampleResponse;
+import com.training.questionaire.qmanager.web.dto.TellMeHelloRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/qmanager")
@@ -24,5 +24,15 @@ public class HelloWorldRestController {
         response.setUser("someUser");
         return ResponseEntity.ok().body(response);
     }
+
+    @PostMapping(value="/tellMeHello")
+    public ResponseEntity<ExampleResponse> tellMeHelloHandlerJSONtoXML(@RequestBody TellMeHelloRequest request){
+        ExampleResponse response= new ExampleResponse();
+        response.setMessageKey("qmanager.message.hello-world."+request.getUsername()+"."+request.getAge());
+        response.setValue(200);
+        response.setUser(request.getUsername());
+        return ResponseEntity.ok().body(response);
+    }
+
 
 }
